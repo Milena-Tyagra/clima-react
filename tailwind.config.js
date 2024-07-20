cb65f83eb7c302b3060a644d244831ca3e32bd43
Milena Tyagra <milenatyagra.sc@gmail.com>
2024-07-20 12:13:36 -0300

    atualização de arquivos iniciais configurações gerais

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
